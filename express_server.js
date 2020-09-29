62d7fda4aee13352d8092fbe1379907196bb0059
Daya <dayanayak30@gmail.com>
2020-09-29 00:24:07 +0000

    Adding body-parser dependency

--- express_server.js
+++ express_server.js
@@ -8,6 +8,8 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req,res) => {
   res.send('Hello!');
